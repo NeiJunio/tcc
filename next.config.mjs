2423e7255b97bfa5a3150dc12997d3516eda2abe
Nei Junio Nogueira Gomes <152228340+NeiJunio@users.noreply.github.com>
2024-04-25 22:48:07 -0300

    corrigir uso da tag image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['assets.example.com'],
+      },
+}
+
 
 export default nextConfig;
+
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,11 +4,50 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Teste next</h1>
-      <Link href={'/sobre'}>clique aqui</Link>
-
-      {/* COLOCA O TEXTO AQUI */}
-    </main>
-  );
+    <header className={styles.header}>
+      <h1> teste</h1>
+      <nav className={styles.navbar}>
+        <div className={styles.logo}>
+          <Image
+            src="https://avatars.githubusercontent.com/u/152228340?v=4"
+            width={500}
+            height={500}
+            alt="Picture of the author"
+          />
+        </div>
+      </nav>
+    </header>
+  )
 }
+
+
+{/* <nav class="nav-bar">
+<div class="logo">
+    <Image src="logo4.png" alt="logo" class="img">
+    
+</div>
+<div class="nav-list">
+    <ul>
+        <li class="nav-item"><a href="#" class="nav-link">INICIO</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">SOBRE</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">SERVIÇOS</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">SOCIAL</a></li>
+    </ul>
+</div>
+
+<div class="mobile-menu-icon">
+    <button onclick="menuShow()"><img class="icon" src="menu_white_36dp (1).svg" alt=""></button>
+</div>
+</nav>
+
+<div class="mobile-menu">
+<ul>
+<li class="nav-item"><a href="#" class="nav-link">INICIO</a></li>
+<li class="nav-item"><a href="#" class="nav-link">SOBRE</a></li>
+<li class="nav-item"><a href="#" class="nav-link">SERVIÇOS</a></li>
+<li class="nav-item"><a href="#" class="nav-link">SOCIAL</a></li>
+</ul>
+
+</div>
+</div>
+< /header> */}
