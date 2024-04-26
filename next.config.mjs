f7e08fc503d520e9460402129ddba6e98a60829b
Nei Junio Nogueira Gomes <152228340+NeiJunio@users.noreply.github.com>
2024-04-26 15:41:18 -0300

    teste do uso de imagens

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['assets.example.com'],
+        formats: ['image/avif', 'image/webp'],
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'images.unsplash.com'
+          }
+        ]
       },
 }
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,18 +2,19 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "./page.module.css";
 
+//  import logo4 from "public/logo4.png";
+
 export default function Home() {
   return (
     <header className={styles.header}>
-      <h1> teste</h1>
       <nav className={styles.navbar}>
         <div className={styles.logo}>
-          <Image
-            src="https://avatars.githubusercontent.com/u/152228340?v=4"
-            width={500}
-            height={500}
-            alt="Picture of the author"
-          />
+          {/* <Image
+            src={"https://images.unsplash.com/photo-1676288176869-b2e1c6bea1a4?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+            width={100}
+            height={100}
+          /> */}
+          <img src={'/img/logo4.png'} className={styles.logo} alt="logo" />
         </div>
       </nav>
     </header>
