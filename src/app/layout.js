ed08b296558f50c56266b40f8c5db9fbf3aa2996
NeiJunio <neijunio50@gmail.com>
2024-05-01 13:19:07 -0300

    commit
    tentativa de carrosel

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import "./globals.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body >
+
+        {children}</body>
     </html>
   );
 }
